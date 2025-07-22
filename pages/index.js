179fcdea3513dffccd40b6648e673b3bebcbee84
Caio André <72115163+caioandre182@users.noreply.github.com>
2025-07-22 01:59:18 +0000

    adicionando pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home(){
+    return (
+        <h1>Não é a nota que você toca que está errada, é a nota que você toca depois que a torna certa ou errada. (Miles Davis)</h1>
+    )
+}
+
+export default Home;
