e8ca0a9dcb3fdffd02962e6827f7d518ffa826c1
Caio André <72115163+caioandre182@users.noreply.github.com>
2025-07-23 02:11:22 +0000

    trocando a tag para h3

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 function Home(){
     return (
-        <h1>Não é a nota que você toca que está errada, é a nota que você toca depois que a torna certa ou errada.</h1>
+        <h3>Não é a nota que você toca que está errada, é a nota que você toca depois que a torna certa ou errada.</h3>
     )
 }
 
