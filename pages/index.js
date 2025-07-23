15047d8785a74e045625c0491d55d9e01c10c97c
Caio André <72115163+caioandre182@users.noreply.github.com>
2025-07-23 02:32:02 +0000

    inserindo a frase correta

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 function Home(){
     return (
-        <h3>Não é a nota que você toca que está errada, é a nota que você toca depois que a torna certa ou errada.</h3>
+        <>
+        <h3>Se você soubesse que está a 30 erros da sua meta.
+            O quão rápido você ia querer fracassar?
+        </h3>
+        <h4 style={{ marginTop: '1rem'}}>
+          Toda vez que eu olho pra você, eu penso, eu preciso ficar rico, eu tenho certeza que você vai conquistar o mundo,
+          e o mínimo que eu preciso, é ter outro mundo te esperando aqui, você merece tudo o que tem de melhor nessa e em todas as outras vidas!
+        </h4>
+        <h5>Amar só Deus!</h5>
+        </>
+
     )
 }
 
