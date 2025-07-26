b36d927804a54b312ce4546188a48e7a92e6c64a
Caio André <72115163+caioandre182@users.noreply.github.com>
2025-07-26 20:33:41 +0000

    adiciona  scripts  e

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-function Home(){
-    return (
-        <>
-        <h3>Se você soubesse que está a 30 erros da sua meta.
-            O quão rápido você ia querer fracassar?
-        </h3>
-        <h4 style={{ marginTop: '1rem'}}>
-          Jamais tenha medo de errar, de não arriscar, você merece o melhor dessa e de todas as outras vidas,
-           não vejo a hora de ter ver feliz de novo
-        </h4>
-        <h5>Amar só a Deus!</h5>
-        </>
-
-    )
+function Home() {
+  return (
+    <>
+      <h3>
+        Se você soubesse que está a 30 erros da sua meta. O quão rápido você ia
+        querer fracassar?
+      </h3>
+      <h4 style={{ marginTop: "1rem" }}>
+        Jamais tenha medo de errar, de não arriscar, você merece o melhor dessa
+        e de todas as outras vidas, não vejo a hora de ter ver feliz de novo
+      </h4>
+      <h5>Amar só a Deus!</h5>
+    </>
+  );
 }
 
 export default Home;
